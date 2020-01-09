4abbf891dee5f749ead8098a39640df1fd21e475
krypton <krypn22@gmail.com>
2020-01-09 23:25:23 +0630

    add ignore list for css / html

--- controllers/StyleEngine.js
+++ controllers/StyleEngine.js
@@ -6,6 +6,7 @@ sass.compiler = require('node-sass')
 const less = require('gulp-less')
 const stylus = require('gulp-stylus')
 const host = require('gulp-connect')
+const gulpif = require('gulp-if')
 
 const StyleEngine = async function () {
   fs.readFile('./cc-config.json', 'utf-8', (err, data) => {
@@ -13,24 +14,19 @@ const StyleEngine = async function () {
     let res = JSON.parse(data)
     let css = res['css']
     let watch = css['watch']
-    if ((css['lang'] === 'scss' && watch.endsWith('.scss')) || css['lang'] === 'sass' && watch.endsWith('.sass')) {
-      return gulp.src(watch)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(dest(`${res['output']}/${css['dist']}`))
-        .pipe(host.reload())
-    } else if (css['lang'] === 'less' && watch.endsWith('.less')) {
-      return gulp.src(watch)
-        .pipe(less())
-        .pipe(dest(`${res['output']}/${css['dist']}`))
-        .pipe(host.reload())
-    } else if (css['lang'] === 'styl' && watch.endsWith('.styl')) {
-      return gulp.src(watch)
-        .pipe(stylus())
-        .pipe(dest(`${res['output']}/${css['dist']}`))
-        .pipe(host.reload())
-    } else {
-      return console.log('your style lang is not supported or watch extension is not correct! \nPlease check your cc-config.json')
-    }
+    let validWatch = watch.filter(ext => !(ext.endsWith(css['lang'])))
+
+    // validate rule
+    if (validWatch.length > 0) return console.log('watch and lang is not match.\nPlease check at cc-config.json')
+
+    if (!['scss', 'sass', 'less', 'styl'].includes(css['lang'])) return console.log('Sorry, your lang is not supported right now!')
+
+    return gulp.src(watch)
+      .pipe(gulpif(css['lang'] === 'scss' || css['lang'] === 'sass', sass().on('error', sass.logError)))
+      .pipe(gulpif(css['lang'] === 'less', less()))
+      .pipe(gulpif(css['lang'] === 'styl', stylus()))
+      .pipe(dest(`${res['output']}/${css['dist']}`))
+      .pipe(host.reload())
   })
 }
 
--- controllers/TemplateEngine.js
+++ controllers/TemplateEngine.js
@@ -6,7 +6,8 @@ const pug = require('gulp-pug')
 const ejs = require('gulp-ejs')
 const mustache = require('gulp-mustache')
 const underscore = require('gulp-underscore-template')
-const host = require("gulp-connect");
+const host = require('gulp-connect');
+const gulpif = require('gulp-if')
 
 const TemplateEngine = async function () {
   fs.readFile('./cc-config.json', 'utf-8', (err, data) => {
@@ -14,32 +15,21 @@ const TemplateEngine = async function () {
     let res = JSON.parse(data)
     let html = res['html']
     let watch = html['watch']
-    if (html['lang'] === 'pug' && watch.endsWith('.pug')) {
-      return gulp.src(watch)
-        .pipe(pug())
-        .pipe(dest(`${res['output']}/${html['dist']}`))
-        .pipe(host.reload())
-    } else if (html['lang'] === 'ejs' && watch.endsWith('.ejs')) {
-      return gulp.src(watch)
-        .pipe(ejs({ title: 'gulp-ejs' }))
-        .pipe(rename({extname : '.html'}))
-        .pipe(dest(`${res['output']}/${html['dist']}`))
-        .pipe(host.reload())
-    } else if (html['lang'] === 'mustache' && watch.endsWith('.mustache')) {
-      return gulp.src(watch)
-        .pipe(mustache())
-        .pipe(rename({extname : '.html'}))
-        .pipe(dest(`${res['output']}/${html['dist']}`))
-        .pipe(host.reload())
-    } else if (html['lang'] === 'underscore' && watch.endsWith('.underscore')) {
-      return gulp.src(watch)
-        .pipe(underscore())
-        .pipe(rename({extname : '.html'}))
-        .pipe(dest(`${res['output']}/${html['dist']}`))
-        .pipe(host.reload())
-    } else {
-      return console.log('your template format is not supported or watch extension is not correct! \nPlease check your cc-config.json')
-    }
+    let validWatch = watch.filter(ext => !(ext.endsWith(html['lang'])))
+
+    // validate rule
+    if (validWatch.length > 0) return console.log('watch and lang is not match.\nPlease check at cc-config.json')
+
+    if (!['pug', 'ejs', 'mustache', 'underscore'].includes(html['lang'])) return console.log('Sorry, your makeup lang is not supported right now!')
+
+    return gulp.src(watch)
+      .pipe(gulpif(html['lang'] === 'pug', pug()))
+      .pipe(gulpif(html['lang'] === 'ejs', ejs({ title: 'gulp-ejs' })))
+      .pipe(gulpif(html['lang'] === 'mustache', mustache()))
+      .pipe(gulpif(html['lang'] === 'underscore', underscore()))
+      .pipe(rename({ extname: '.html' }))
+      .pipe(dest(`${res['output']}/${html['dist']}`))
+      .pipe(host.reload())
   })
 }
 
